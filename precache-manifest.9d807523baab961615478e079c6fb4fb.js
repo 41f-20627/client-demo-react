28e8a567d5fa872681115ce196886608b16999db
Jonathan Martel <jonathmartel@gmail.com>
2021-09-21 15:11:25 -0400

    version démo

---
+++ precache-manifest.9d807523baab961615478e079c6fb4fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73ea10e6ec622ea30d6129664767dc8",
+    "url": "//127.0.0.1:8000/client/index.html"
+  },
+  {
+    "revision": "c55b8b0a816bcea0a146",
+    "url": "//127.0.0.1:8000/client/static/css/main.87232074.chunk.css"
+  },
+  {
+    "revision": "fa5ceea366f3539631ef",
+    "url": "//127.0.0.1:8000/client/static/js/2.79601149.chunk.js"
+  },
+  {
+    "revision": "c55b8b0a816bcea0a146",
+    "url": "//127.0.0.1:8000/client/static/js/main.cb3419ad.chunk.js"
+  },
+  {
+    "revision": "c8b51df4964e9723f4b6",
+    "url": "//127.0.0.1:8000/client/static/js/runtime-main.b57acf5c.js"
+  }
+]);
